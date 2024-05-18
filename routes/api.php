36e1f996681bbf5e827786eb72e9d4f5655f73a9
Richard Alvarez <richardandrewalvarez1207@gmail.com>
2024-05-18 15:34:04 -0500

    Creacion de Rutas Paymode y Producto

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\api\CategoriaController;
 use App\Http\Controllers\api\CustomerController;
+use App\Http\Controllers\api\ProductController;
+use App\Http\Controllers\api\PaymodeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
@@ -14,7 +16,7 @@ Route::get('/user', function (Request $request) {
 Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias');
 Route::post('/categorias', [CategoriaController::class, 'store'])->name('categorias.store');
 Route::delete('/categorias/{categoria}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
-Route::get('/categorias/{categoria}/edit', [CategoriaController::class, 'show'])->name('categorias.show');
+Route::get('/categorias/{categoria}', [CategoriaController::class, 'show'])->name('categorias.show');
 Route::put('/categorias/{categoria}', [CategoriaController::class, 'update'])->name('categorias.update');
 
 //Rutas de Customers
@@ -22,9 +24,21 @@ Route::put('/categorias/{categoria}', [CategoriaController::class, 'update'])->n
 Route::get('/customers', [CustomerController::class, 'index'])->name('customers');
 Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
 Route::delete('/customers/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
-Route::get('/customers/{customer}/edit', [CustomerController::class, 'show'])->name('customers.show');
+Route::get('/customers/{customer}', [CustomerController::class, 'show'])->name('customers.show');
 Route::put('/customers/{customer}', [CustomerController::class, 'update'])->name('customers.update');
 
 //Rutas de Paymode
 
+Route::get('/paymodes', [PaymodeController::class, 'index'])->name('paymodes');
+Route::post('/paymodes', [PaymodeController::class, 'store'])->name('paymodes.store');
+Route::delete('/paymodes/{paymode}', [PaymodeController::class, 'destroy'])->name('paymodes.destroy');
+Route::get('/paymodes/{paymode}', [PaymodeController::class, 'show'])->name('paymodes.show');
+Route::put('/paymodes/{paymode}', [PaymodeController::class, 'update'])->name('paymodes.update');
 
+//Rutas Productos
+
+Route::get('/productos', [PaymodeController::class, 'index'])->name('paymodes');
+Route::post('/productos', [PaymodeController::class, 'store'])->name('paymodes.store');
+Route::delete('/productos/{producto}', [PaymodeController::class, 'destroy'])->name('paymodes.destroy');
+Route::get('/productos/{producto}', [PaymodeController::class, 'show'])->name('paymodes.show');
+Route::put('/productos/{producto}', [PaymodeController::class, 'update'])->name('paymodes.update');
