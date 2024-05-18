bb75884a25f2cd85bdb24104efcb83ecede4b2bd
Richard Alvarez <richardandrewalvarez1207@gmail.com>
2024-05-18 14:56:49 -0500

    Creacion de Customers

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,29 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\api\CategoriaController;
+use App\Http\Controllers\api\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//Rutas de Categorias
+
 Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias');
 Route::post('/categorias', [CategoriaController::class, 'store'])->name('categorias.store');
 Route::delete('/categorias/{categoria}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
 Route::get('/categorias/{categoria}/edit', [CategoriaController::class, 'show'])->name('categorias.show');
 Route::put('/categorias/{categoria}', [CategoriaController::class, 'update'])->name('categorias.update');
+
+//Rutas de Customers
+
+Route::get('/customers', [CustomerController::class, 'index'])->name('customers');
+Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
+Route::delete('/customers/{customer}', [CustomerController::class, 'destroy'])->name('customers.destroy');
+Route::get('/customers/{customer}/edit', [CustomerController::class, 'show'])->name('customers.show');
+Route::put('/customers/{customer}', [CustomerController::class, 'update'])->name('customers.update');
+
+//Rutas de Paymode
+
+
